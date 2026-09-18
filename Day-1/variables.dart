5608a3da0c2a620212b2b8a537c26a2f16f23007
Anusha <anushashetty932@gmail.com>
2026-09-18 23:37:24 +0530

    Add variables and print statements in Dart
    Day 1: Learned Dart variables

---
+++ Day-1/variables.dart
@@ -0,0 +1,11 @@
+void main() {
+  String name = "Anusha";
+  int age = 20;
+  double height = 5.5;
+  bool isLearningFlutter = true;
+
+  print(name);
+  print(age);
+  print(height);
+  print(isLearningFlutter);
+}

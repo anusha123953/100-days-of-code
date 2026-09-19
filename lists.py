c59a98b4cc95bb33808d7ac369760e926351a9e0
Anusha <anushashetty932@gmail.com>
2026-09-19 20:07:18 +0530

    Implement list operations and demonstrate usage
    Added various list operations including insert, sort, reverse, and sum. Demonstrated usage of index, count, and pop methods on lists.

--- lists.py
+++ lists.py
@@ -1,1 +1,76 @@
+items = ["Bruh", "Sugar", "Milk"]
+print(items)
 
+items.insert(1, "coffe")
+print(items)
+
+items[0] = "Coffe powder"
+print(items)
+
+l = [100, 200, 300, 400,500]
+print(l[0::2])
+
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
+
+
+numbers = [5, 2, 9, 1]
+print(sorted(numbers))
+
+
+numbers = [45, 76, 86, 90]
+print(sorted(numbers))
+print(numbers)
+
+numbers = [87, 76, 101, 54]
+print(sorted(numbers))
+
+numbers = [1 , 2, 3, 4]
+print(sum(numbers))
+
+numbers = [20, 30, 40, 50]
+print(sum(numbers))
+
+fruits = ["apple", "banana", "mango", "orange"]
+print(fruits.index("apple"))
+print(fruits.index("banana"))
+
+numbers = [100, 200, 300, 400, 100]
+print(numbers.count(100))
+
+fruits = ["apple", "banana", "mango", "orange"]
+fruits.reverse()
+print(fruits)
+
+fruits = ["gobi", "panipuri", "gadbad", "allupuri"]
+fruits.reverse()
+print(fruits)
+
+numbers = [5, 2, 9, 1]
+numbers.sort()
+numbers.sort(reverse=True)
+print(numbers)
+
+
+matrix = [
+[1, 2, 3],
+[4, 5, 6],
+[7, 8, 9]
+]
+print(matrix[0])
+print(matrix[1])
+print(matrix[1][1])
+
+fruits.pop()
+print(fruits)
+
+fruits.pop(0)
+print(fruits)
+
+numbers = [5,2, 9,1,7]
+numbers.sort(reverse=True)
+print(numbers)
+
+numbers = [1, 2, 3, 4,5]
+numbers.reverse()
+print(numbers)

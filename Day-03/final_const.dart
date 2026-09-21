371dd47fc1e6d17df514e839157e36b4af4a68b2
Anusha <anushashetty932@gmail.com>
2026-09-21 16:59:09 +0530

    Demonstrate usage of final and const in Dart
    Added examples of 'final' and 'const' in Dart.

--- Day-03/final_const.dart
+++ Day-03/final_const.dart
@@ -1,1 +1,11 @@
+void main() {
+  // final - value can be assigned only once
+  final String name = "Akshit";
 
+  print(name);
+
+  // const - compile-time constant
+  const double pi = 3.14;
+
+  print(pi);
+}

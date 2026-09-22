6f9a0fa4b4c392b1c3f68d257b35c0592337cc9b
Anusha <anushashetty932@gmail.com>
2026-09-22 23:57:47 +0530

    Add tuple examples and operations in tuples.py
    Added examples demonstrating tuple creation, indexing, length, and methods.

---
+++ tuples.py
@@ -0,0 +1,41 @@
+numbers = (10, 20, 30, 40)
+print(numbers)
+
+student = ("Anusha", 20, 8.5, True)
+print(student)
+
+x = (10,)
+print(type(x))
+
+numbers = (10, 20, 30, 40)
+
+print(numbers[0])
+print(numbers[2])
+
+numbers = (10, 20, 30, 40)
+
+print(numbers[0])
+print(numbers[2])
+
+numbers = (10, 20, 30, 40)
+
+print(numbers[-1])
+print(numbers[-2])
+
+numbers = (10, 20, 30)
+
+# numbers[0] = 100  ❌ Error
+
+numbers = (10, 20, 30, 40)
+
+print(len(numbers))
+
+numbers = (10, 20, 20, 30, 20)
+
+print(numbers.count(20))
+print(numbers.index(30))
+
+numbers = (10, 20, 30, 40)
+
+for num in numbers:
+    print(num)

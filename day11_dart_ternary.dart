099dcc1767b35f5e1e66e99b3d10962fdafa7851
Anusha <anushashetty932@gmail.com>
2026-09-25 21:18:49 +0530

    Add examples of ternary operator usage in Dart
    This file demonstrates the use of ternary operators in Dart for age, even/odd checks, and pass/fail evaluations.

---
+++ day11_dart_ternary.dart
@@ -0,0 +1,19 @@
+void main() {
+  // Example 1: Check age
+  int age = 20;
+
+  String ageResult = age >= 18 ? "Adult" : "Minor";
+  print(ageResult);
+
+  // Example 2: Check even or odd
+  int number = 10;
+
+  String numberResult = number % 2 == 0 ? "Even" : "Odd";
+  print(numberResult);
+
+  // Example 3: Check pass or fail
+  int marks = 75;
+
+  String result = marks >= 40 ? "Pass" : "Fail";
+  print(result);
+}

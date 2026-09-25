300e2f1af625fe77a0a214514edab210cb5664ee
Anusha <anushashetty932@gmail.com>
2026-09-25 21:14:51 +0530

    Implement ternary operator examples in Dart

---
+++ day11_ternary_operator.dart
@@ -0,0 +1,19 @@
+void main() {
+  int age = 20;
+
+  String result = age >= 18 ? "Adult" : "Minor";
+
+  print(result);
+
+  int number = 10;
+
+  String check = number % 2 == 0 ? "Even" : "Odd";
+
+  print(check);
+
+  int marks = 75;
+
+  String grade = marks >= 40 ? "Pass" : "Fail";
+
+  print(grade);
+}
